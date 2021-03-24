abf3a6fc603e6c9ff35bcaab0f67ce9a9d013826
tawandalloyd <tawandalloydcharuka@gmail.com>
2021-03-24 13:45:35 +0200

    sign/signup

--- app/src/main/java/com/example/translator/MainActivity.kt
+++ app/src/main/java/com/example/translator/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.translator
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.translator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
